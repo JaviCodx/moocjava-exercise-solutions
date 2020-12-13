519d32429bdaac6b1ec9aaa98aef15c889162574
Javier Saiz <javiersaizcano@gmail.com>
2020-12-13 16:22:31 +0100

    Fixed Part06_13

---
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/Exercise.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author javicodx
+ */
+public class Exercise {
+
+    private String name;
+    private boolean completed;
+
+    public Exercise(String exerciseName) {
+        this.name = exerciseName;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
--- mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/MainProgram.java
@@ -1,8 +1,9 @@
 
+
 public class MainProgram {
 
     // update here your exercise progress
     public static int partsCompleted() {
-        return 0;
+        return 2;
     }
 }
